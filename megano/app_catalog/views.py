cebe93989fee054ab95cd5e6833cc3bda8192f94
Татьяна <tanika135@mail.ru>
2023-05-30 19:24:21 +0300

    список товаров

--- megano/app_catalog/views.py
+++ megano/app_catalog/views.py
@@ -8,59 +8,44 @@ from app_catalog.models import Product, Category, ProductReviews, Tag
 
 
 def categories(request):
-    categories = Category.objects.filter(parent=None)
-    data = []
-    for category in categories:
-        subcategories = []
-        for subcategory in category.children.all():
-            subcategories.append({
-                "id": subcategory.id,
-                "title": subcategory.title,
+    if request.method == 'GET':
+        categories = Category.objects.filter(parent=None)
+        data = []
+        for category in categories:
+            subcategories = []
+            for subcategory in category.children.all():
+                subcategories.append({
+                    "id": subcategory.id,
+                    "title": subcategory.title,
+                    # "image": {
+                    #     "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+                    #     "alt": "Image alt string"
+                })
+
+            data.append({
+                "id": category.id,
+                "title": category.title,
+                "subcategories": subcategories,
                 # "image": {
                 #     "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
                 #     "alt": "Image alt string"
             })
 
-        data.append({
-            "id": category.id,
-            "title": category.title,
-            "subcategories": subcategories,
-            # "image": {
-            #     "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-            #     "alt": "Image alt string"
-        })
-
-    return JsonResponse(data, safe=False)
+        return JsonResponse(data, safe=False)
 
 
 def catalog(request):
+    products = Product.objects.all()
+    product_list = []
+    for product in products:
+        product_data = get_product_data(product)
+        product_data["reviews"] = product.reviews.count()
+        product_list.append(
+            product_data
+        )
+
     data = {
-        "items": [
-             {
-                 "id": 123,
-                 "category": 123,
-                 "price": 500.67,
-                 "count": 12,
-                 "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
-                 "title": "video card",
-                 "description": "description of the product",
-                 "freeDelivery": True,
-                 "images": [
-                        {
-                            "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-                            "alt": "hello alt",
-                        }
-                 ],
-                 "tags": [
-                        {
-                            "id": 0,
-                            "name": "Hello world"
-                        }
-                 ],
-                 "reviews": 5,
-                 "rating": 4.6
-             }
-        ],
+        "items": product_list,
         "currentPage": randrange(1, 4),
         "lastPage": 3
     }
@@ -70,43 +55,11 @@ def catalog(request):
 def product(request, id):
     product = get_product(id)
     if request.method == 'GET':
-        product_images = []
-        for image in product.images.all():
-            product_images.append({
-                "src": image.images.url,
-                "alt": product.title,
-            })
-
-        product_tags = []
-        for tag in product.tags.all():
-            product_tags.append({
-                'id': tag.pk,
-                'name': tag.name
-            })
-
-        reviews_on_product = get_reviews(product)
-        data = {
-            "id": product.id,
-            "category": product.category.id,
-            "price": product.price,
-            "count": product.count,
-            "date": product.date,
-            "title": product.title,
-            "description": product.description,
-            "fullDescription": product.fullDescription,
-            "freeDelivery": product.freeDelivery,
-            "images": product_images,
-            "tags": product_tags,
-            "reviews": reviews_on_product,
-            "specifications": [
-                {
-                    "name": "Size",
-                    "value": "XL"
-                }
-            ],
-            "rating": 4.6
-        }
+        data = get_product_data(product)
+        data["reviews"] = get_reviews(product)
         return JsonResponse(data)
+    else:
+        return HttpResponse(status=400)
 
 
 def get_product(product_id):
@@ -115,6 +68,56 @@ def get_product(product_id):
         return None
     else:
         return product
+
+
+def get_product_images(product):
+    product_images = []
+    for image in product.images.all():
+        product_images.append({
+            "src": image.images.url,
+            "alt": product.title,
+        })
+    if not product_images:
+        product_images.append({
+            "src": '/media/no_image.jpg',
+            "alt": product.title,
+        })
+    return product_images
+
+
+def get_product_tags(product):
+    product_tags = []
+    for tag in product.tags.all():
+        product_tags.append({
+            'id': tag.pk,
+            'name': tag.name
+        })
+    return product_tags
+
+
+def get_product_data(product):
+    product_images = get_product_images(product)
+    data = {
+        "id": product.id,
+        "category": product.category.id,
+        "price": product.price,
+        "count": product.count,
+        "date": product.date,
+        "title": product.title,
+        "description": product.description,
+        "fullDescription": product.fullDescription,
+        "freeDelivery": product.freeDelivery,
+        "images": product_images,
+        "tags": get_product_tags(product),
+        "specifications": [
+            {
+                "name": "Size",
+                "value": "XL"
+            }
+        ],
+        "rating": 4.6
+    }
+    return data
 
 
 def get_reviews(product:Product) -> list:
