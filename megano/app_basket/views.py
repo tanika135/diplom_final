1cc064ee0514cd18ea15821a3032822252e5a64b
Татьяна <tanika135@mail.ru>
2023-06-01 00:31:57 +0300

    Добавление в корзину

--- megano/app_basket/views.py
+++ megano/app_basket/views.py
@@ -1,92 +1,37 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import json
+from app_basket.cart import Cart
+from app_catalog.models import Product
+from app_catalog.views import get_product_data
+
+
+def get_cart_data(cart):
+    data = []
+    for item in cart:
+        product_data = get_product_data(item['product'])
+        product_data["count"] = item["quantity"]
+        data.append(product_data)
+    return data
 
 
 def basket(request):
+    cart = Cart(request)
     if request.method == "GET":
-        print('[GET] /api/basket/')
-        data = [
-            {
-                "id": 123,
-                "category": 55,
-                "price": 500.67,
-                "count": 12,
-                "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
-                "title": "video card",
-                "description": "description of the product",
-                "freeDelivery": True,
-                "images": [
-                    {
-                        "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-                        "alt": "hello alt",
-                    }
-                ],
-                "tags": [
-                    {
-                        "id": 0,
-                        "name": "Hello world"
-                    }
-                ],
-                "reviews": 5,
-                "rating": 4.6
-            },
-            {
-                "id": 124,
-                "category": 55,
-                "price": 201.675,
-                "count": 5,
-                "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
-                "title": "video card",
-                "description": "description of the product",
-                "freeDelivery": True,
-                "images": [
-                    {
-                        "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-                        "alt": "hello alt",
-                    }
-                ],
-                "tags": [
-                    {
-                        "id": 0,
-                        "name": "Hello world"
-                    }
-                ],
-                "reviews": 5,
-                "rating": 4.6
-            }
-        ]
+        data = get_cart_data(cart)
         return JsonResponse(data, safe=False)
 
     elif request.method == "POST":
         body = json.loads(request.body)
         id = body['id']
-        count = body['count']
-        print('[POST] /api/basket/   |   id: {id}, count: {count}'.format(id=id, count=count))
-        data = [
-            {
-                "id": id,
-                "category": 55,
-                "price": 500.67,
-                "count": 13,
-                "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
-                "title": "video card",
-                "description": "description of the product",
-                "freeDelivery": True,
-                "images": [
-                    {
-                        "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-                        "alt": "hello alt",
-                    }
-                ],
-                "tags": [
-                    {
-                        "id": 0,
-                        "name": "Hello world"
-                    }
-                ],
-                "reviews": 5,
-                "rating": 4.6
-            }
-        ]
+        count = int(body['count'])
+
+        product = Product.objects.get(pk=id)
+        if not product:
+            return HttpResponse(status=400)
+
+        cart.add(product, count)
+        cart.save()
+        data = get_cart_data(cart)
         return JsonResponse(data, safe=False)
 
     elif request.method == "DELETE":
