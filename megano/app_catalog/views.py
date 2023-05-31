0db23ede5de88c9ed84c082eddfdac7778adc7ae
Татьяна <tanika135@mail.ru>
2023-05-31 22:20:09 +0300

    приложение корзины и заказа

--- megano/app_catalog/views.py
+++ megano/app_catalog/views.py
@@ -267,33 +267,34 @@ def tags(request):
 
 
 def products_popular(request):
-
-    data = [
-        {
-            "id": "123",
-            "category": 55,
-            "price": 500.67,
-            "count": 12,
-            "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
-            "title": "video card",
-            "description": "description of the product",
-            "freeDelivery": True,
-            "images": [
-                    {
-                        "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
-                        "alt": "hello alt",
-                    }
-             ],
-            "tags": [
-                {
-                    "id": 0,
-                    "name": "Hello world"
-                }
-             ],
-            "reviews": 5,
-            "rating": 4.6
-        }
-    ]
+    if request.method == 'GET':
+        popular_products = []
+    # data = [
+    #     {
+    #         "id": "123",
+    #         "category": 55,
+    #         "price": 500.67,
+    #         "count": 12,
+    #         "date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+    #         "title": "video card",
+    #         "description": "description of the product",
+    #         "freeDelivery": True,
+    #         "images": [
+    #                 {
+    #                     "src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+    #                     "alt": "hello alt",
+    #                 }
+    #          ],
+    #         "tags": [
+    #             {
+    #                 "id": 0,
+    #                 "name": "Hello world"
+    #             }
+    #          ],
+    #         "reviews": 5,
+    #         "rating": 4.6
+    #     }
+    # ]
     return JsonResponse(data, safe=False)
 
 
