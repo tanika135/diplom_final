79d4f25ff345855b8b118b69b36384cb40b8b4c5
Татьяна <tanika135@mail.ru>
2023-06-01 22:11:17 +0300

    корзина в бд

--- megano/app_basket/cart.py
+++ megano/app_basket/cart.py
@@ -1,65 +1,57 @@
-from decimal import Decimal
-from django.conf import settings
-from app_catalog.models import Product
+from app_basket.models import CartItem
 
 
-class Cart(object):
-
+class Cart:
     def __init__(self, request):
         """ Инициализируем корзину """
-        self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
-        if not cart:
-            cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
-        """ Добавить продукт в корзину или обновить его количество. """
-        product_id = str(product.id)
-        if not product_id in self.cart:
-            self.cart[product_id] = 0
+        if not request.session.session_key:
+            request.session.create()
 
-        if update_quantity:
-            self.cart[product_id] = quantity
-        else:
-            self.cart[product_id] += quantity
-        self.save()
-
-    def save(self):
-        """ Обновление сессии cart """
-        self.session[settings.CART_SESSION_ID] = self.cart
-        """ Отметить сеанс как "измененный", чтобы убедиться, что он сохранен """
-        self.session.modified = True
-
-    def remove(self, product):
-        """ Удаление товара из корзины. """
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
+        self.cart_user_id = request.session.session_key
 
     def __iter__(self):
         """ Перебор элементов в корзине и получение продуктов из базы данных. """
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        for product in products:
+        items = CartItem.objects.filter(cart_id=self.cart_user_id)
+        for item in items:
             item = {
-                'product': product,
-                'quantity': self.cart[str(product.id)],
+                'product': item.product,
+                'quantity': item.quantity,
             }
             yield item
 
+    def add(self, product, quantity=1, update_quantity=False) -> int:
+        """ Добавить продукт в корзину или обновить его количество. """
+        try:
+            product_in_cart = CartItem.objects.get(cart_id=self.cart_user_id, product=product)
+            if update_quantity:
+                product_in_cart.quantity = quantity
+            else:
+                product_in_cart.quantity += quantity
+            product_in_cart.save()
+
+        except CartItem.DoesNotExist:
+            product_in_cart = CartItem.objects.create(cart_id=self.cart_user_id, product=product,
+                                                      quantity=quantity, price=product.price)
+
+        return product_in_cart.pk
+
+    def remove(self, product) -> bool:
+        """ Удаление товара из корзины. """
+        try:
+            product_in_cart = CartItem.objects.get(cart_id=self.cart_user_id, product=product)
+            product_in_cart.delete()
+            return True
+        except CartItem.DoesNotExist:
+            return False
+
+    def clear(self) -> bool:
+        """ удаление корзины """
+        items = CartItem.objects.filter(cart_id=self.cart_user_id)
+        for item in items:
+            item.delete()
+        return True
+
     def __len__(self):
         """ Подсчет всех товаров в корзине. """
-        return len(self.cart)
-
-    # def get_total_price(self):
-    #     """ Подсчет стоимости товаров в корзине. """
-    #     return sum(Decimal(item['price']) * self.get_item_quantity(item) for item in
-    #                self.cart.values())
-
-    def clear(self):
-        """ удаление корзины из сессии. """
-        del self.session[settings.CART_SESSION_ID]
-        self.session.modified = True
-
+        return CartItem.objects.filter(cart_id=self.cart_user_id).count()
--- megano/app_basket/models.py
+++ megano/app_basket/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from app_catalog.models import Product
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    cart_id = models.CharField(max_length=100, blank=False, null=False)
